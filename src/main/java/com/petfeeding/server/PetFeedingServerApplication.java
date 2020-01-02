01fa1f3288a4572775298a4982a33b3ff208c7b2
zijianno1 <pkunewking@126.com>
2020-01-02 22:12:24 +0800

    initial commit

---
+++ src/main/java/com/petfeeding/server/PetFeedingServerApplication.java
@@ -0,0 +1,13 @@
+package com.petfeeding.server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PetFeedingServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PetFeedingServerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/petfeeding/server/PetFeedingServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.petfeeding.server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PetFeedingServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
