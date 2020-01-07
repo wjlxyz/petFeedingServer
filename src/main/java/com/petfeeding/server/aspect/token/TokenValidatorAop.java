0c9b18a57e1e731e219f1603d05889c071c65594
jinlong <jinlongwang@etekcity.com.cn>
2020-01-07 13:40:50 +0800

    fix token validation aspect

--- src/main/java/com/petfeeding/server/aspect/token/TokenValidatorAop.java
+++ src/main/java/com/petfeeding/server/aspect/token/TokenValidatorAop.java
@@ -6,7 +6,7 @@ import java.lang.annotation.*;
  * @author jinlong
  * @since 2020-01-06 15:08
  */
-@Target(ElementType.METHOD)
+@Target({ElementType.METHOD})
 @Retention(RetentionPolicy.RUNTIME)
 @Documented
 public @interface TokenValidatorAop {
--- src/main/java/com/petfeeding/server/aspect/token/TokenValidatorAspect.java
+++ src/main/java/com/petfeeding/server/aspect/token/TokenValidatorAspect.java
@@ -7,10 +7,9 @@ import com.petfeeding.server.dto.response.ApiResponse;
 import com.petfeeding.server.dto.response.result.token.TokenResult;
 import com.petfeeding.server.service.TokenService;
 import lombok.extern.slf4j.Slf4j;
-import org.aspectj.lang.ProceedingJoinPoint;
-import org.aspectj.lang.annotation.Around;
+import org.aspectj.lang.JoinPoint;
 import org.aspectj.lang.annotation.Aspect;
-import org.aspectj.lang.annotation.Pointcut;
+import org.aspectj.lang.annotation.Before;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.annotation.Order;
 import org.springframework.stereotype.Component;
@@ -30,18 +29,12 @@ public class TokenValidatorAspect {
 
     private static Gson gson = new Gson();
 
-    @Pointcut("@within(com.petfeeding.server.aspect.token.TokenValidatorAop)")
-    public void handleTokenValidatorPointCut() {
-    }
-
-    @Around(value = "handleTokenValidatorPointCut()")
-    public void validateToken(ProceedingJoinPoint point) throws Throwable {
+    @Before(value = "@annotation(com.petfeeding.server.aspect.token.TokenValidatorAop)")
+    public void validateToken(JoinPoint point) throws Throwable {
         log.info("validate token");
         ApiRequest request = (ApiRequest) point.getArgs()[0];
         ApiResponse<TokenResult> response = tokenService.validateToken(request);
-        if (response.getErrorCode().isSuccess()) {
-            point.proceed();
-        } else {
+        if (!response.getErrorCode().isSuccess()) {
             throw new RuntimeException(ErrorCode.COMMON_TOKEN_EXPIRED_ERROR.getMsg());
         }
     }
